8dd52590f3db56a577b4f7b054d4f671cf289236
Andrew Morozov <morozovmav@gmail.com>
2017-04-06 22:31:52 +0300

    Improved types handling

--- index.ts
+++ index.ts
@@ -5,6 +5,74 @@ export interface Builder<TWrapper, TObject> {
 	unwrap: (name: string, func: (...args: any[]) => (context: TObject) => any) => Builder<TWrapper, TObject>
 };
 
+interface Ctor<T> { new (context: T): Instance<T>; }
+interface Instance<T> { value: T; }
+
+interface Cascadable<T> { (...args: any[]): (ctx: T) => void }
+interface Chainable<T> { (...args: any[]): (ctx: T) => T };
+
+let cascade = <T>(name: string, ctor: Ctor<T>, func: Cascadable<T>) => {
+	ctor.prototype[name] = function(this: Instance<T>, ...args: any[]) {
+		func(...args)(this.value);
+		return this;
+	};
+};
+
+// TODO: can't make it T1->T2, because I need to change type of this after operation. How? :\
+let chain = <T>(name: string, ctor: Ctor<T>, func: Chainable<T>) => {
+	ctor.prototype[name] = function(this: Instance<T>, ...args: any[]){
+		this.value = func(...args)(this.value);
+		return this;
+	};
+};
+
+class Builder2<T> implements Instance<Ctor<T>> {
+	public value: Ctor<T>;
+	constructor(context: Ctor<T>) {
+		this.value = context;
+	}
+}
+
+interface IBuilder2<T> {
+	chain: (name: string, func: Chainable<T>) => IBuilder2<T> & Builder2<T>;
+}
+
+cascade("cascade", Builder2, (name: string, func: Cascadable<any>) => (ctx) => {
+	cascade(name, ctx, func);
+});
+cascade("chain", Builder2, (name: string, func: Chainable<any>) => (ctx) => {
+	chain(name, ctx, func);
+})
+
+class Calc implements Instance<number> {
+	public value: number;
+	constructor(context: number) {
+		this.value = context;
+	}
+}
+
+interface ICalc {
+	add: (b: number) => ICalc & Calc;
+	sub: (b: number) => ICalc & Calc;
+	add2: (b: number) => ICalc & Calc;
+	sub2: (b: number) => ICalc & Calc;
+}
+
+// adding "add" and "sub" directly
+chain("add", Calc, (b) => (a) => a + b);
+chain("sub", Calc, (b) => (a) => a + b);
+
+// adding "add2" and "sub2" using builder
+((<IBuilder2<number> & Builder2<number>>new Builder2(Calc))
+	.chain("add2", (b) => (a) => a + b)
+	.chain("sub2", (b) => (a) => a - b)
+	.value);
+
+let calc = <ICalc & Calc>new Calc(5);
+console.log(calc.add(6).add(7).value);
+let calc2 = <ICalc & Calc>new Calc(5);
+console.log(calc2.add2(6).add2(7).value);
+
 export let build = <TWrapper, TObject>() => {
 	class Wrapper {
 		public value: TObject;
